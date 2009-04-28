16ddb39b503017282cdf8403669dc2fbae4b3aa8
marco_b <marco_b@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-28 21:10:14 +0000

    initial import
    git-svn-id: https://plugins.svn.wordpress.org/linksextractor/trunk@114169 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ linksextractor.php
@@ -0,0 +1,61 @@
+<?php
+	/*
+Plugin Name: Linksextractor
+Plugin URI: http://plugins.wirtschaftsinformatiker.cc
+Description: Place all your Links at the bottom of your post, automatically
+Author: Marco Bischoff
+Version: 0.3
+Author URI: http://wirtschaftsinformatiker.cc
+*/ 
+
+if(function_exists('load_plugin_textdomain'))
+	load_plugin_textdomain('linksextractor', false, dirname(plugin_basename(__FILE__)) . '/languages');
+
+
+	function linksextracter_set_urls($content) {
+		$sContent = $content;
+		$sZeile = $content;
+		$aLinks = array();
+		$sPattern = '=^(.*)<a(.*)href\="?(\S+)"([^>]*)>(.*)</a>(.*)$=msi';
+		$sNew = $sContent;
+		$iCounter = 0;
+	
+		while (preg_match($sPattern, $sZeile, $sTxt)) {
+		   $aLinks[$iCounter] = array();
+		   $aLinks[$iCounter][0] = $sTxt[3];
+		   $aLinks[$iCounter][1] = $sTxt[4];
+		   $aLinks[$iCounter][2] = $sTxt[5];
+		
+		   $sZeile =  $sTxt[1]." hier war mal ein Link ".$sTxt[6];
+		   $iCounter++;
+
+		}
+		
+		asort($aLinks, SORT_NUMERIC);
+		$iCounter = 1;
+		
+		foreach($aLinks as $sValue) {
+			$sOld = $sValue[0]."\"".$sValue[1].">".$sValue[2]."</a>";
+			$sRep = $sValue[0]."\"".$sValue[1].">".$sValue[2]."</a> [".$iCounter."]";
+			
+			$sNew = str_replace($sOld , $sRep  ,$sNew);
+			$iCounter++;
+		}
+	
+		echo $sNew;
+		if(count($aLinks) != 0) {
+			echo "<div class=\"links\">\n<p>".__('Quelle', 'linksextractor')."</p>\n<ul>\n";
+		
+			$iCounter = 1;
+			
+			foreach($aLinks as $sValue) {	
+				echo "<li> [".$iCounter."] ".$sValue[0]."</li>";
+				$iCounter++;
+			}
+			
+			echo "</ul>\n</div>";
+		}
+	}
+
+	add_action('the_content', 'linksextracter_set_urls');
+?>
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== Linkextractor ===
+Tags: links, extractor, list, print version
+Requires at least: 2.0.0
+Tested up to: 2.2
+Stable tag: 0.3
+
+This plugin extract the links in your post and lists them under your post. Your links in the post will not be removed.
+
+== Description ==
+
+This plugin print all links in a post at the bottom of the post. This is usefull when you would like to have a print version, 
+so without this plugin you see on the paper only the underlined link but, you don't know where the link is going to.
+
+The linkextractor solv this problem. After installing the plugin it will print out the links at the bottom and also on your printed version.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+
+1. Upload `linkextractor.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Enjoy!
