38f9446c2dc3ba2a920ce9f3e2ba963459cafd76
marco_b <marco_b@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-13 06:52:17 +0000

    Fixed some bugs
    git-svn-id: https://plugins.svn.wordpress.org/linksextractor/trunk@117598 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- linksextractor.php
+++ linksextractor.php
@@ -12,62 +12,72 @@ if(function_exists('load_plugin_textdomain'))
 	load_plugin_textdomain('linksextractor', false, dirname(plugin_basename(__FILE__)) . '/languages');
 
 
-	function linksextracter_set_urls($content) {
+	function linksextractor_set_urls($content) {
+		global $wp_query;
+		$iStatus = get_post_meta($wp_query->post->ID, '_linkextracter_is_set', true);
+		
 		$sContent = $content;
-		$sZeile = $content;
-		$aLinks = array();
-		$sPattern = '=^(.*)<a(.*)href\="?(\S+)"([^>]*)>(.*)</a>(.*)$=msi';
-		$sNew = $sContent;
-		$iCounter = 0;
-	
-		while (preg_match($sPattern, $sZeile, $sTxt)) {
-		   $aLinks[$iCounter] = array();
-		   $aLinks[$iCounter][0] = $sTxt[3];
-		   $aLinks[$iCounter][1] = $sTxt[4];
-		   $aLinks[$iCounter][2] = $sTxt[5];
+		if($iStatus == 0) {
+			$sZeile = $content;
+			$aLinks = array();
+			$sPattern = '=^(.*)<a(.*)href\="?(\S+)"([^>]*)>(.*)</a>(.*)$=msi';
+			$sNew = $sContent;
+			$iCounter = 0;
 		
-		   $sZeile =  $sTxt[1]." hier war mal ein Link ".$sTxt[6];
-		   $iCounter++;
+			while (preg_match($sPattern, $sZeile, $sTxt)) {
+			   $aLinks[$iCounter] = array();
+			   $aLinks[$iCounter][0] = $sTxt[3];
+			   $aLinks[$iCounter][1] = $sTxt[4];
+			   $aLinks[$iCounter][2] = $sTxt[5];
+			
+			   $sZeile =  $sTxt[1]." hier war mal ein Link ".$sTxt[6];
+			   $iCounter++;
 
-		}
-		
-		asort($aLinks, SORT_NUMERIC);
-		$iCounter = 1;
-		
-		foreach($aLinks as $sValue) {
-			$sOld = $sValue[0]."\"".$sValue[1].">".$sValue[2]."</a>";
-			$sRep = $sValue[0]."\"".$sValue[1].">".$sValue[2]."</a> [".$iCounter."]";
-			
-			$sNew = str_replace($sOld , $sRep  ,$sNew);
-			$iCounter++;
-		}
-	
-		echo $sNew;
-		if(count($aLinks) != 0) {
-			echo "<div class=\"links\">\n<p>".__('Quelle', 'linksextractor')."</p>\n<ul>\n";
-		
-			$iCounter = 1;
-			
-			foreach($aLinks as $sValue) {	
-				echo "<li> [".$iCounter."] ".$sValue[0]."</li>";
-				$iCounter++;
 			}
 			
-			echo "</ul>\n</div>";
+			asort($aLinks, SORT_NUMERIC);
+			$iCounter = 1;
+			
+			foreach($aLinks as $sValue) {
+				$sOld = $sValue[0]."\"".$sValue[1].">".$sValue[2]."</a>";
+				$sRep = $sValue[0]."\"".$sValue[1].">".$sValue[2]."</a> [".$iCounter."]";
+				
+				$sNew = str_replace($sOld , $sRep  ,$sNew);
+				$iCounter++;
+			}
+		
+			echo $sNew;
+			
+			if(count($aLinks) != 0) {
+				echo "<div class=\"links\">\n<p>".__('Quelle', 'linksextractor')."</p>\n<ul>\n";
+			
+				$iCounter = 1;
+				
+				foreach($aLinks as $sValue) {	
+					echo "<li> [".$iCounter."] ".$sValue[0]."</li>";
+					$iCounter++;
+				}
+				
+				echo "</ul>\n</div>";
+			}
+		}
+		else {
+		 echo $sContent;
 		}
 	}
 
 	function linksextractor_get_prop() {
 		$iStatus = get_post_meta($_GET["post"], '_linkextracter_is_set', true);
+		
 		$out = '<label for="linkextractor">';
 		$out .= '<select id="linkextractor" name="linkextractor"> ';
 		if($iStatus == 0) {
 			$out .= '<option selected="selected" value="0">'.__("Links extrahieren", "linksextractor").'</option>  ';
 		} else {
-			$out .= '<option selected="selected" value="0">'.__("Links extrahieren", "linksextractor").'</option>  ';
+			$out .= '<option value="0">'.__("Links extrahieren", "linksextractor").'</option>  ';
 		}
 
-		if($Status == 1) {
+		if($iStatus == 1) {
 			$out .= '<option selected="selected" value="1">'.__("Links nicht extrahieren", "linksextractor").'</option>  ';
 		} else {
 			$out .= '<option value="1">'.__("Links nicht extrahieren", "linksextractor").'</option>  ';
@@ -94,6 +104,6 @@ function linksextractor_special_admin_init() {
 	 add_action('edit_post','linksextractor_set_prop');
 	}
 	
-	add_action('admin_menu','linksextracter_special_admin_init');
-	add_action('the_content', 'linksextracter_set_urls');
+	add_action('admin_menu','linksextractor_special_admin_init');
+	add_action('the_content', 'linksextractor_set_urls');
 ?>
